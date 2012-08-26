ec0cfb93e1af15c38545bb842a6b1c15a86cc5fa
mikach27 <mikach@gmail.com>
2012-08-26 09:47:45 +0300

    coomit-n

--- js/dynamic.js
+++ js/dynamic.js
@@ -13,10 +13,9 @@ Dynamic = function(height, width) {
 	console.log("Field created: "+w+'x'+h);
 	$('div.field').css({'width':w,'height':h});	
 
-	speed = 5;
+	speed = 5;	
+	angle = 0;//-Math.PI/2.5;
 	$ball = $('.ball');
-	moveX = 'right';
-	moveY = 'down';
 }
 
 Dynamic.prototype.check = function() {
@@ -26,41 +25,44 @@ Dynamic.prototype.check = function() {
 	var t2 = $('.player2').position().top;
 	var l1 = $('.player1').position().left;
 	var l2 = $('.player2').position().left;
-	// lets check the goal
+	
 	if ((l+16>=w) && (t>75) && (t<225)) {
 		n = parseInt($('.score span').eq(1).html());
 		$('.score span').eq(1).html(n+1);
-		$ball.css({'top':'0px','left':'390px'});
-		moveX = 'left';
-		moveY = 'down';
+		$ball.css({'top':'2px','left':'380px'});
+		angle = 5*Math.PI/4;
 		return false;
 	}
 	if ((l<=1) && (t>75) && (t<225)) {
 		n = parseInt($('.score span').eq(0).html());
 		$('.score span').eq(0).html(n+1);
-		$ball.css({'top':'0px','left':'0px'});
-		moveX = 'right';
-		moveY = 'down';
+		$ball.css({'top':'2px','left':'2px'});
+		angle = -Math.PI/4;
 		return false;
 	}
-	// lets check the ball's position
-	if (t >= h-16) moveY = 'up';
-	if (t <= 1) moveY = 'down';
-	if ( (l >= w-16) || ( (l+16 >= l2) && (t>=t2) && (t+16<t2+40) && (l-5<l2) ) )
-		moveX = 'left';
-	if ((l <= 1) || ( (l <= l1+1) && (t>=t1) && (t+16<t1+40) && (l>l1-5) ) ) 
-		moveX = 'right';
-	if (moveY == 'down') {
-		t += speed;
-	} else {
-		t -= speed;
+	if ( ((l+15>=w)||(l<=1)) && ( (t<75)||(t>255) )) {
+		angle = Math.PI-angle;		
 	}
-	if (moveX == 'right') {
-		l += speed;
-	} else {
-		l -= speed;
+	
+	if ((t >= h-16) || (t <= 1)) angle = -angle;
+	
+	if ((t>t1-10) && (t<t1+40)) {
+		var an1 = 90-parseFloat( $('.player1').rotate() );
+		var k1 = Math.tan(-an1*Math.PI/180);
+		var c1 = t1-k1*l1;
+		var r1 = Math.abs((t)+(-k1*l)-c1)/Math.sqrt(Math.pow(k1,2)+1);
+		if (Math.abs(r1) <= 4) angle = -angle+an1;
 	}
-//$ball.stop().animate({'top':t+'px','left':l+'px'},100);
+	if ((t>t2-10) && (t<t2+40)) {
+		var an2 = 90-parseFloat( $('.player2').rotate() );
+		var k2 = Math.tan(-an2*Math.PI/180);
+		var c2 = t2-k2*l2;
+		var r2 = Math.abs((t)+(-k2*l)-c2)/Math.sqrt(Math.pow(k2,2)+1);
+		if (Math.abs(r2) <= 4) angle = -angle+an2;
+	}
+
+	t -= speed*Math.sin(angle);
+	l += speed*Math.cos(angle);
 	$ball.css({'top':t+'px','left':l+'px'});
 }
 
